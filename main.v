093344398d87324a56df137cd6387bada73a2ccc
avey <avey777@163.com>
2022-09-03 11:04:23 +0800

    0.0.2

--- main.v
+++ main.v
@@ -3,9 +3,12 @@ import net.http
 import json
 
 fn main() {
+
 	// a := qsqlqueryu('2','5') or {return}
-	sqldata := sqlquery('1','100000')?
-	println(sqldata)
+	// sqldata := sqlquery('1','10')?
+	// println(sqldata)
+
+	sqldata := mysql_orm('1','10')
 
 
 	// data := '{"id": "6204", "tsin": "SPU000280COEY"}'
@@ -14,6 +17,22 @@ fn main() {
 	// s := request(data)?
 	println(reponse)
 }
+
+fn request(data string) ?string {
+	mut req := http.Request{
+		method: http.Method.post
+		url: "http://192.168.3.2:7700/indexes/tospinomall/documents"
+		// data: "{'id': '6204', 'tsin': 'SPU000280COEY'}" // 此样式不可用
+		// data: '{"id": "6204", "tsin": "SPU000280COEY"}'
+		data: data
+	}
+	req.add_custom_header('Content-Type', 'application/json')?
+	req.add_custom_header('Authorization', 'Bearer aveyamie')?
+	reponse := req.do()?
+	// println("打印req:$reponse")
+	return reponse.body
+}
+
 pub fn sqlquery(startpoint string, numperpage string) ?[]map[string]string {
 	mut conn := mysql.Connection{
 		host: '192.168.3.2'
@@ -28,11 +47,12 @@ pub fn sqlquery(startpoint string, numperpage string) ?[]map[string]string {
 	mut sqlstr := 'select id,id,tsin, good_title,main_picture_url,keyword,good_code,ifnull(seller_id,0),ifnull(shop_id,0),' +
 	'ifnull(brand_id,0),ifnull(category_id,0),goods_type,ifnull(is_cash_delivery,0),ifnull(delivery_type,0),'+
 	'transport_mode from tospinomall_product.pms_goods_info limit '+startpoint+','+numperpage
-	
+
 	mut res := conn.query(sqlstr)?
+	// println(res)
 	mut mapstrlist := []map[string]string{} //创建空数组
 	for row in res.rows() {
-		//map的另一种写法
+		// map的另一种写法
 		// direct := {
 		// 	'id': row.vals[0]
 		// 	'tsin': row.vals[1]
@@ -55,24 +75,66 @@ pub fn sqlquery(startpoint string, numperpage string) ?[]map[string]string {
 		direct["transport_mode"] = row.vals[14]
 		// println(direct)
 		mapstrlist << direct  //追加direct到mapstrlist 数组
-	}	
+	}
 	conn.close()
 	return mapstrlist
 }
 
-
-fn request(data string) ?string {
-	mut req := http.Request{
-		method: http.Method.post
-		url: "http://192.168.3.2:7700/indexes/tospinomall/documents"
-		// data: "{'id': '6204', 'tsin': 'SPU000280COEY'}" // 此样式不可用
-		// data: '{"id": "6204", "tsin": "SPU000280COEY"}'
-		data: data
-	}
-	req.add_custom_header('Content-Type', 'application/json')?
-	req.add_custom_header('Authorization', 'Bearer aveyamie')?
-	reponse := req.do()?
-	// println("打印req:$reponse")
-	return reponse.body
+[table: 'pms_goods_info']
+struct Module {
+	id           int    [primary; sql: serial]
+	tsin         string
+	good_title	string
+	main_picture_url	string
+	keyword	string
+	good_code	string
+	seller_id 	int
+	shop_id		int
+	brand_id	int
+	category_id	int
+	goods_type	int
+	is_cash_delivery int
+	delivery_type int
+	transport_mode int
 }
 
+
+fn mysql_orm(startpoint string, numperpage string) []map[string]string {
+	mut conn := mysql.Connection{
+		host: '10.243.0.2'
+		port: 3306
+		username: 'admin'
+		password: 'admin'
+		dbname: 'tospinomall_product'
+	}
+	conn.connect() or { panic(err) }
+
+	res := sql conn {
+		select from Module limit startpoint+','+numperpage
+	}
+	// eprintln('$row.id')
+	mut mapstrlist := []map[string]string{} //创建空数组
+	for row in res {
+		mut direct := map[string]string{} // a map with `string` keys and `string` values
+		direct["id"] = '$row.id'
+		direct["objectID"] = '$row.id'
+		direct["tsin"] = '$row.tsin'
+		direct["good_title"] = '$row.good_title'
+		direct["poster"] = '$row.main_picture_url'+ "?.webp"  //不增加后缀，meilisearch不显示图片
+		direct["keyword"] = '$row.keyword'
+		direct["good_code"] = '$row.good_code'
+		direct["seller_id"] = '$row.seller_id'
+		direct["shop_id"] = '$row.shop_id'
+		direct["brand_id"] = '$row.brand_id'
+		direct["category_id"] = '$row.category_id'
+		direct["goods_type"] = '$row.goods_type'
+		direct["is_cash_delivery"] = '$row.is_cash_delivery'
+		direct["delivery_type"] = '$row.delivery_type'
+		direct["transport_mode"] = '$row.transport_mode'
+		// println(direct)
+		mapstrlist << direct  //追加direct到mapstrlist 数组
+	}
+	// println(mapstrlist)
+	conn.close()
+	return mapstrlist
+}
