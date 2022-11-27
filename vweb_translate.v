f5b315c23ab19db50b7dc0482a7d2c91f057ad37
avey <avey777@163.com>
2022-11-27 19:01:58 +0800

    0.0.8

--- vweb_translate.v
+++ vweb_translate.v
@@ -1,17 +1,26 @@
-// fn JS.Math.tan(f64) f64
-// fn JS.console.log(string)
-// fn tan(x f64) f64 {
-//     return f64(JS.Math.tan(x)) // do not forget to wrap return value in `f64` again, atm js gen does not do this automatically
-// }
-// JS.console.log("hello from js!")
-
+// blog.v
+module main
+import vweb
 import net.http
 import json
 
-fn main(){
+
+struct App {
+	vweb.Context
+}
+
+fn main() {
+	app := App{}
+	vweb.run(app, 8081)
+}
+
+['/index']
+pub fn (mut app App) index() !vweb.Result {
 	r := request()!
 	mut jsonstr := json.encode(r)
 	println(jsonstr)
+
+	return app.json(jsonstr)
 }
 
 fn request() !string {
@@ -25,10 +34,6 @@ fn request() !string {
 		method: http.Method.get
 		url: 'https://translate.google.com/translate_a/single?client=webapp&sl=zh-CN&tl=en&hl=zh-CN&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=sos&dt=ss&dt=t&otf=2&ssel=0&tsel=0&kc=3&tk=' + get_tk + '&q=' + q
 
-		// url: "https://ms-43c9c269b6b3-327.lon.meilisearch.io/indexes/tospinomall/documents"
-		// data: "{'id': '6204', 'tsin': 'SPU000280COEY'}" // 此样式不可用
-		// data: '{"id": "6204", "tsin": "SPU000280COEY"}'
-		// data: data
 	}
 	// req.add_custom_header('Content-Type', 'application/json')!
 	req.add_custom_header('user-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36')!
